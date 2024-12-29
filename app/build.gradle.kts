604dfcc6380f42c99d01cbb14aa1cd2ffd5954e1
ymm1m <89432413+ymm1m@users.noreply.github.com>
2024-12-29 22:14:44 +0900

    簡易GUI作成

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+//    id("com.google.devtools.ksp") version "2.1.0-1.0.29"
 }
 
 android {
@@ -45,4 +46,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+//    implementation("androidx.room:room-runtime:2.6.1")
+//    ksp("androidx.room:room-compiler:2.6.1")
+//    implementation("androidx.room:room-ktx:2.6.1")
 }
--- app/src/main/java/com/ymm1m/mylog/MainActivity.kt
+++ app/src/main/java/com/ymm1m/mylog/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.ymm1m.mylog
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,32 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener(this::onButtonClick)
+    }
+
+    /*
+     * Called when the button is clicked.
+     * Roomに、日時と位置情報を保存する
+     */
+    fun onButtonClick(view: View) {
+//        val currentTimestamp = System.currentTimeMillis()
+//        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+//            if (location != null) {
+//                val currentTimestamp = System.currentTimeMillis()
+//                val locationLog = LocationLog(
+//                    timestamp = currentTimestamp,
+//                    latitude = location.latitude,
+//                    longitude = location.longitude
+//                )
+//
+//                lifecycleScope.launch(Dispatchers.IO) {
+//                    locationLogDao.insert(locationLog)
+//                }
+//            }
+//        }
+
+        Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
     }
 }
